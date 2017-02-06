b4db3acb7ced5754807ab4168fa3c882980c148c
llopez005 <lopez477@morris.umn.edu>
2017-02-06 14:13:16 -0600

    have the layout for tests complete.

--- src/test/javascript/todo.spec.js
+++ src/test/javascript/todo.spec.js
@@ -1,3 +1,18 @@
 /**
  * Created by lopez477 on 2/1/17.
  */
+describe('testing todo functions', function(){
+    it('should return the correct ID', function(){
+        expect(testStringLength("kittens")).toEqual(7);
+    });
+
+    it('returnKittens should return kittens', function(){
+        expect(returnKittens()).toBe("kittens");
+    });
+
+    ////this test should fail.
+    it('this test should fail!', function(){
+        expect(returnKittens()).toBe("Not Kittens!");
+    });
+});
+
